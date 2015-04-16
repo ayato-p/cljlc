675f3c4ec0442db4970b772c4adf22f44d7a4d63
ayato_p <ayato.skycrawlers@gmail.com>
2015-04-16 02:22:45 -0700

    add more test

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :repositories {"Atilika Open Source repository" "http://www.atilika.org/nexus/content/repositories/atilika"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.atilika.kuromoji/kuromoji "0.7.7"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins      [[lein-midje "3.1.3"]]}})
--- src/cljlc/core.clj
+++ src/cljlc/core.clj
@@ -3,7 +3,23 @@
   (:require [clojure.string :as str]))
 
 (def ^:private ignore-chars
-  #"[\.\,\s\!\"\$\%\'\(\)\*\+\-\:\;\<\=\>\?\@\[\¥\]\^\_\`\{\|\}\~\#\&\/、。；：’”＜＞？・「」｛｝＋＝＿ー）（＊＆＾％＄＃＠！〜｀]")
+  #"[\.\,\s\!\"\$\%\'\(\)\*\+\-\:\;\<\=\>\?\@\[\¥\]\^\_\`\{\|\}\~\#\&\/、。；：’”＜＞？・「」｛｝＋＝＿ー）（＊＆＾％＄＃＠！〜｀…☆]")
+
+(def ^:private small-chars
+  #"[ァィゥェォヵヶャュョッ]")
+
+(def ^:private convert-map
+  {"ァ" "ア"
+   "ィ" "イ"
+   "ゥ" "ウ"
+   "ェ" "エ"
+   "ォ" "オ"
+   "ヵ" "カ"
+   "ヶ" "ケ"
+   "ャ" "ヤ"
+   "ュ" "ユ"
+   "ョ" "ヨ"
+   "ッ" "ツ"})
 
 (defn- filter-ignore-chars [s]
   (str/join
@@ -12,6 +28,9 @@
 
 (defn- only-alphabet? [s]
   (re-matches #"^[\w\.\,\s\!\"\$\%\'\(\)\*\+\-\:\;\<\=\>\?\@\[\¥\]\^\_\`\{\|\}\~\#\&\/]+$" s))
+
+(defn- jp-small->large [s]
+  (str/replace s small-chars #(get convert-map %)))
 
 (defn- jp-src->reading [src]
   (let [tokenizer (.build (Tokenizer/builder))]
@@ -24,8 +43,10 @@
          str/join)))
 
 (defn palindrome? [s]
-  (let [converter (comp (if (only-alphabet? s) str/lower-case jp-src->reading)
-                        filter-ignore-chars)
+  (let [converter (comp filter-ignore-chars
+                        (if (only-alphabet? s)
+                          str/lower-case
+                          #(-> % jp-src->reading jp-small->large)))
         converted-str (converter s)
         reversed-str (str/reverse converted-str)]
     (= converted-str reversed-str)))
